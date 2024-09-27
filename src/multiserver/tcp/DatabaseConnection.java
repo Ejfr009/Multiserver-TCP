d2b1410036209139458e9fe0b24617a6b086f25b
Jazmín Madeline Irazusta Frutos <118125638+JazminIrazusta@users.noreply.github.com>
2024-09-27 14:46:03 -0400

    base de datos

---
+++ src/multiserver/tcp/DatabaseConnection.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Jazmin
+ */
+package multiserver.tcp;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:postgresql://localhost:5432/TCP_server";
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "passw"; 
+
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null || connection.isClosed()) {
+            try {
+                Class.forName("org.postgresql.Driver");
+                connection = DriverManager.getConnection(URL, USER, PASSWORD);
+            } catch (ClassNotFoundException e) {
+                throw new SQLException(e);
+            }
+        }
+        return connection;
+    }
+}

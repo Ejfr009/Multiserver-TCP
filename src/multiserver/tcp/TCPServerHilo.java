214e61e571b6f4520398da575a8458f3c9ca396b
EJFR09 <134091229+Ejfr009@users.noreply.github.com>
2024-09-29 19:33:13 -0400

    Cambios 5

--- src/multiserver/tcp/TCPServerHilo.java
+++ src/multiserver/tcp/TCPServerHilo.java
@@ -7,29 +7,102 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
+
+
 
 
 public class TCPServerHilo extends Thread {
 
-    private Socket socket = null;
+    private Socket socket;
+    private EstructuraDatos datos;
+    private ObjectOutputStream out; 
+    private ObjectInputStream in; 
+    private boolean respuesta;
+
     
     public TCPServerHilo(Socket socket) {
         super("TCPServerHilo");
         this.socket = socket;
+        CrearConexion();
+        this.respuesta = false; 
+        
     }
     
-    private boolean validar(EstructuraDatos datos)
+    
+    @Override
+    public void run() {
+        
+        
+        while(true)
+        {
+            login();
+            logout();
+        }
+           
+        //solucionar el while infinito
+        //CerrarConexion();
+    }
+    
+    
+    private void login()
+    {
+        try{
+            while(!respuesta)
+            {
+
+                datos = (EstructuraDatos) in.readObject();
+                validar(datos);
+
+                out.writeObject(respuesta);
+                out.reset();
+            }
+        }catch( IOException | ClassNotFoundException e){
+            System.err.println("Error de login");
+            System.exit(1);
+        }
+        
+        this.respuesta = false; 
+    }
+    
+    private void logout()
+    {
+      
+   
+    }
+    
+    private void CrearConexion()
+    {
+        
+        try {
+            this.out = new ObjectOutputStream(socket.getOutputStream());
+            this.in = new ObjectInputStream(socket.getInputStream()); 
+        } catch (IOException ex) {
+            System.err.println("Error de Stream");
+            System.exit(1);
+        }
+        
+    }
+    
+    private void CerrarConexion()
+    {
+        try {
+            if (in != null) in.close();
+            if (out != null) out.close();
+            if (socket != null) socket.close();
+        } catch (IOException ex) {
+            System.err.println("Error de Stream");
+            System.exit(1);
+        }
+    }
+    
+    
+    private void validar(EstructuraDatos datos)
     {
         String username = datos.getUser();
         String password = datos.getPassword();
-        
-        System.out.println(username);
-        System.out.println(password);
-        boolean respuesta = false;  
-         
      
+         
         try (Connection conn = DatabaseConnection.getConnection()) {
             String query = "SELECT * FROM users WHERE username = ? AND password = ?";
     
@@ -39,50 +112,15 @@ public class TCPServerHilo extends Thread {
         
                 try (ResultSet rs = stmt.executeQuery()) {
                     if (rs.next()) {
-                        respuesta = true;  // Credenciales válidas
+                        this.respuesta = true;  // Credenciales válidas
                     }
                 }
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }                  
-            
-        return respuesta;
-    }
 
-    @Override
-    public void run() {
-        
-        EstructuraDatos datos;
-
-        try {
-            // Recibir el objeto serializado con los datos del cliente
-            ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
-            ObjectInputStream in = new ObjectInputStream(socket.getInputStream());
-            
-            while(true)
-            {
-                datos = (EstructuraDatos) in.readObject(); 
-            
-                boolean respuesta = validar(datos);
-                
-                out.writeObject(respuesta);
-                out.reset();
-            }
-        }
-        catch (UnknownHostException e) 
-        {
-            System.err.println("Host desconocido");
-            System.exit(1);
-        } 
-        catch (IOException e)
-        {
-            System.err.println("Error de I/O en la conexion al host");
-            System.exit(1);
-        } catch (ClassNotFoundException ex) {
-           System.err.println("Clase de objeto no encontrada");
-           System.exit(1);
-        }
     }
+    
 }
 
